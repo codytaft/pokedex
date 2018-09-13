51fe06f57d1ca9325437e6f33cb8a7a273ac8c9d
Cody Taft <cody.taft@gmail.com>
2018-09-13 15:10:58 -0600

    Pass initial state  pokemonreducer test

---
+++ src/reducers/pokemon-reducer.test.js
@@ -0,0 +1,23 @@
+import { pokemonReducer } from './pokemon-reducer';
+import * as actions from '../actions';
+
+describe('pokemonReducer', () => {
+  it('should return the intial state', () => {
+    const expected = [];
+
+    const result = pokemonReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+  it('should reutn the state with new pokemon', () => {
+    const pokemon = [
+      {
+        name: 'spearow',
+        id: 21,
+        type: 3,
+        weight: 20,
+        sprites: []
+      }
+    ];
+  });
+});
